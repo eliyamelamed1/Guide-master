87fbbc9c67e06edb57fc66d10978c963621b6dc7
eliyamelamed1 <eliyamelamed1@gmail.com>
2020-11-15 00:02:28 +0200

    .

--- Back-end/Projects/django-for-beginners/ch16-deployment/accounts/views.py
+++ Back-end/Projects/django-for-beginners/ch16-deployment/accounts/views.py
@@ -1,12 +1,10 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .forms import CustomUserCreationForm
 
 # creating sign up module - all of the other modules are build in Django 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm 
     success_url = reverse_lazy('login') # redirect to login when sign up is finished
     template_name = 'registration/signup.html'
-
-
