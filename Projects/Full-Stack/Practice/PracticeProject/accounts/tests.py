9dd8a365b3fdcd4bad4d65c377021a39119a1f94
eliyamelamed1 <eliyamelame1@gmail.com>
2021-01-05 19:49:37 +0200

    added test classes

--- Projects/Full-Stack/Practice/PracticeProject/accounts/tests.py
+++ Projects/Full-Stack/Practice/PracticeProject/accounts/tests.py
@@ -3,11 +3,11 @@ import conftest
 
 # ------------------------------------------------ Tests
 @pytest.mark.django_db
-def test_signup(create_user):
+def test_signup_success(create_user):
     assert create_user.status_code == 201
 
 
 @pytest.mark.django_db
-def test_login_with_email(login_user):
+def test_login_success(login_user):
     assert login_user.status_code == 200
 
--- Projects/Full-Stack/Practice/PracticeProject/recipes/tests.py
+++ Projects/Full-Stack/Practice/PracticeProject/recipes/tests.py
@@ -2,49 +2,49 @@ import pytest
 import conftest
 
 # ------------------------------------------------ Tests
-@pytest.mark.django_db
-def test_recipes_list_page_doesnt_render_for_guest_users(api_client):
-    recipes_list_url = '/recipes/'
-    page_render = api_client.get(recipes_list_url)
+class Test_authenticated_users:
 
-    assert page_render.status_code == 401
+    @pytest.mark.django_db
+    def test_recipes_list_page_render_for_authenticated_users_success(self, api_client ,login_user):
+        recipes_list_url = '/recipes/'
+        page_render = api_client.get(recipes_list_url)
 
-
-@pytest.mark.django_db
-def test_recipes_list_page_render_for_authenticated_users(api_client ,login_user):
-    recipes_list_url = '/recipes/'
-    page_render = api_client.get(recipes_list_url)
-
-    assert page_render.status_code == 200
+        assert page_render.status_code == 200
 
 
-@pytest.mark.django_db
-def test_create_recipe_page_doesnt_render_for_guest_user(api_client):
-    recipe_creation_url = '/recipes/create/'
-    recipe_creation_url_render = api_client.get(recipe_creation_url)
+    @pytest.mark.django_db
+    def test_create_recipe_page_render_for_authenticated_user_success(self, api_client, login_user):
+        recipe_creation_url = '/recipes/create/'
+        recipe_creation_url_render = api_client.get(recipe_creation_url)
 
-    assert recipe_creation_url_render.status_code == 401
+        # 405 method not allowed - get isnt allowed only post
+        assert recipe_creation_url_render.status_code == 405 
 
 
 
-@pytest.mark.django_db
-def test_create_recipe_page_render_for_authenticated_user(api_client, login_user):
-    recipe_creation_url = '/recipes/create/'
-    recipe_creation_url_render = api_client.get(recipe_creation_url)
+    @pytest.mark.django_db
+    def test_create_recipe_as_authenticated_user_success(self, api_client, login_user, create_recipe):
+        
+        assert create_recipe.status_code == 201
 
-    # 405 method not allowed - get isnt allowed only post
-    assert recipe_creation_url_render.status_code == 405 
+class Test_guest_users:
+    @pytest.mark.django_db
+    def test_recipes_list_page_doesnt_render_for_guest_users_error(self, api_client):
+        recipes_list_url = '/recipes/'
+        page_render = api_client.get(recipes_list_url)
+
+        assert page_render.status_code == 401
 
 
+    @pytest.mark.django_db
+    def test_create_recipe_page_doesnt_render_for_guest_user_error(self, api_client):
+        recipe_creation_url = '/recipes/create/'
+        recipe_creation_url_render = api_client.get(recipe_creation_url)
 
-@pytest.mark.django_db
-def test_create_recipe_as_authenticated_user(api_client, login_user, create_recipe):
+        assert recipe_creation_url_render.status_code == 401
+
     
-    assert create_recipe.status_code == 201
-
-
-@pytest.mark.django_db
-def test_create_recipe_as_guest_user(api_client, create_recipe):
-    
-    assert create_recipe.status_code == 401
-
+    @pytest.mark.django_db
+    def test_create_recipe_as_guest_user_error(self, api_client, create_recipe):
+        
+        assert create_recipe.status_code == 401
